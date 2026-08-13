3635975564fdb7f93c1febcc0d900e9ce97a5a3d
kevinrua2003 <kevinrua2003@gmail.com>
2026-08-13 19:13:55 -0400

    perf(cursor): drop custom cursor engine for native one

--- src/lib/contextCursor/chunks/isElHasProperty.ts
+++
@@ -1,6 +0,0 @@
-import propNames from "../propNames";
-
-const isElHasProperty = (el: HTMLElement, property: string) =>
-  el.getAttribute(propNames.dataAttr)?.includes(property) ?? false;
-
-export default isElHasProperty;
